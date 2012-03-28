4fbe765773c9013dbdff34b9c4132911cabd50f3
Dave Walton <dave.walton@jax.org>
2012-03-28 13:17:25 -0400

    Added copyright.

--- desnp.py
+++ desnp.py
@@ -40,6 +40,21 @@ OPTIONS:
     -z, --zip      a zip containing the probe file. This also assumes there is a
                    file in the zip named probes.tsv, and the file is --tab
     
+  Copyright (c) 2012 The Jackson Laboratory
+  
+  This is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+ 
+  This software is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 import sys
